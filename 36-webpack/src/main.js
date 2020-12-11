3fcbb0c70a5229be86402f5106541df76f3056ba
huang-zp <hzhangpeng@gmail.com>
2020-12-11 14:45:43 +0800

    webpack loader

---
+++ 36-webpack/src/main.js
@@ -0,0 +1,12 @@
+import {sum, mul} from "./js/mathUtils";
+
+console.log(sum(10, 20));
+
+console.log(mul(10, 20));
+
+
+// commonJS语法导入css
+require('./css/normal.css')
+
+require('./css/spec.less')
+document.writeln('<h2>hello</h2>')
